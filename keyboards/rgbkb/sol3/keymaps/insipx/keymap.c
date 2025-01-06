539c8ab136ad4eb2253955ae2c9d66e7463b6cbb
Andrew Plaza <github@andrewplaza.dev>
2025-01-06 15:40:32 -0500

    improvements to config

--- keyboards/rgbkb/sol3/keymaps/insipx/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/insipx/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FN,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_EQL,                   KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  LCTL_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,                  KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
-        KC_LCTL,  RGB_TOG, ADJUST   KC_LGUI, KC_LALT, KC_SPC,  KC_GRV, MOD_MEH, KC_TAB,  KC_ESC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_LBRC, KC_RBRC, KC_RCTL,
+        KC_LCTL,  RGB_TOG, ADJUST,  KC_LGUI, KC_LALT, KC_SPC,  KC_GRV, MOD_MEH, KC_TAB,  KC_ESC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_LBRC, KC_RBRC, KC_RCTL,
 
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
